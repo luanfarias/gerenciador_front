6aa8cfc58ed2083dd248ece67d24fb4d4c45a055
Luan Farias <luanfarias.ip@gmail.com>
2021-02-25 16:04:00 -0300

    criacao do componente pdv

---
+++ src/app/pdv/pdv.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PdvComponent } from './pdv.component';
+
+
+
+@NgModule({
+  declarations: [
+    PdvComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PdvComponent
+  ]
+})
+export class PdvModule { }
